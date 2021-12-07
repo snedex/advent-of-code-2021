0c09dc4cc7545d84edf5363464056d7ae3383f7c
Will <69020009+snedex@users.noreply.github.com>
2021-12-06 23:10:15 -0500

    Setup for day 3

---
+++ 03/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program {
+
+    public static readonly string FileName = "input.txt";
+
+    public static void Main(string[] args)
+    {
+        PartOne();
+    }
+
+    private static void PartOne()
+    {
+
+    }
+}
